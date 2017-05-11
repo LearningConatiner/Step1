4e5b71c9d7a174a80568266829acb72adf5a4738
workwithsri <workwithsrilakshmi@gmail.com>
2017-05-11 20:55:41 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -8,5 +8,9 @@ app = Flask(__name__)
 def hello():
     return 'Hello World from host \"%s\".\n' % socket.gethostname()
 
+@app.route('/sri')
+def hellosri():
+    return 'Hello Sri from  \"%s\".\n' % socket.gethostname()
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=4000, debug=True)
